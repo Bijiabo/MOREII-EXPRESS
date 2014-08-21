6d89630107fa26fb2f124b19b5f785e6e870009c
毕加波 <hu@cafa.me>
2014-08-21 15:01:58 +0800

    新梳理逻辑，更新游戏 /wine/test

--- public/js/app/wine_test.js
+++ public/js/app/wine_test.js
@@ -5,10 +5,79 @@ var cache = {
     score:0,
     imageElement:{}
 };
-
+var itemSize= 120;
+//window.onload=function(){itemWidth= document.getElementsByTagName('canvas')[0].offsetWidth/3}
 Cut(function(root, container) {
     Cut.Mouse(root, container, true);
     root.viewbox(360, 720);
+
+
+    //绘制酒瓶
+    var last = null,
+        clickCount = 0;
+    root.on("viewport", function(width, height) {
+        itemWidth=width/3;
+    });
+
+    var row = Cut.column(0.5).pin({'alignX':0.5,'alignY':1}).appendTo(root);
+    var addLabItem = function(){
+        var line = Cut.row(0.5).appendTo(row);
+        for (var i = 0; i < Math.floor(Math.random()*3+1); i++) {
+            Cut.image("beer:test").pin({
+                scaleMode:'in',
+                scaleWidth:itemSize,
+                scaleHeight:itemSize
+            }).appendTo(line).on(
+                Cut.Mouse.CLICK, function(ev, point) {
+                    if (this != last) {
+                        last = this;
+                        if(!cache.canvasHeight){
+                            cache.canvasHeight = document.getElementsByTagName('canvas')[0].height;
+                        }
+                        if(this.parent().pin('offsetY')+itemSize>=row.pin('height')){
+                            clickCount++;
+                            cache.score++;
+                            this.setImage('beerok:test').pin({
+                                scaleMode:'in',
+                                scaleWidth:itemSize,
+                                scaleHeight:itemSize
+                            });
+                            scoreString.setValue("Score:"+cache.score);
+                            row.tween(duration = 100, delay = 0).clear().pin({
+                                pivotY:-itemSize*clickCount
+                            }).then(function(){
+                                addLabItem();
+                            });
+                        }
+
+                        /*this.tween(duration = 100, delay = 0).clear()
+                            .pin({
+                                scaleX : Cut.Math.random(0.3, 0.4),
+                                scaleY : Cut.Math.random(0.3, 0.4)
+                            })
+                            .then(function(){
+                                clickCount++;
+                                cache.score++;
+                                this.setImage('beerok:test').pin({
+                                    scaleMode:'in',
+                                    scaleWidth:itemSize,
+                                    scaleHeight:itemSize
+                                });
+                                scoreString.setValue("Score:"+cache.score);
+                                row.tween(duration = 100, delay = 0).clear().pin({
+                                    offsetY:-itemSize*clickCount
+                                })
+                                addLabItem();
+                            });*/
+                    }
+
+                    return true;
+                });
+        }
+    }
+    for (var i = 0; i < 1; i++) {
+        addLabItem();
+    }
     //绘制分数
     var scorerow = Cut.row().pin({
         offset : 1,
@@ -26,30 +95,6 @@ Cut(function(root, container) {
     });
     scoreString.setValue("Score:"+cache.score);
 
-
-    //绘制酒瓶
-    var last = null;
-    var colors = [ "green", "blue", "purple", "red", "orange", "yellow" ];
-
-    var row = Cut.row(0.5).appendTo(root).pin("align", 0.5).spacing(1);
-//    var row = Cut.create().appendTo(root);
-    for (var i = 0; i < colors.length; i++) {
-        Cut.image("beer:test").pin({
-            scaleMode:'in',
-            scaleWidth:100,
-            scaleHeight:100
-        }).appendTo(row).on(
-            Cut.Mouse.MOVE, function(ev, point) {
-                if (this != last) {
-                    last = this;
-                    this.tween().clear().pin({
-                        scaleX : Cut.Math.random(0.8, 1.6),
-                        scaleY : Cut.Math.random(0.8, 1.6)
-                    });
-                }
-                return true;
-            });
-    }
 });
 
 /*texture*/
@@ -72,14 +117,30 @@ Cut.addTexture(texture = {
         }
     ]
 });
+Cut.addTexture(texture = {
+    name : 'beerok',
+    imagePath : '/img/wine/labok.png',
+    imageRatio : 1,
+    cutouts : [
+        { // list of cutoutDefs or cutouts
+            name : 'test',
+            x : 0,
+            y : 0,
+            width : 144,
+            height : 144,
+            top : 0,
+            bottom : 0,
+            left : 0,
+            right : 0
+        }
+    ]
+});
 Cut.addTexture({
     name : "base",
     factory : function(name) {
         if (name.substring(0, 2) === "d_") {
             var d = name.substr(2, 1);
-            console.log(d);
             return Cut.Out.drawing("d_" + d, 12, 24, 10, function(ctx, ratio) {
-                console.log(ratio);
                 ctx.scale(ratio, ratio);
                 ctx.font = "bold 24px Arial";
                 ctx.fillStyle = "#000";
